dcbb3c03890e5542385d48d0521761685d7e253a
webdesignetobicoke <webdesignetobicoke@gmail.com>
2024-12-27 22:27:23 -0500

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import Wrapper from "@/components/layout/Wrapper";
+import HomeMain from './(homes)/home-1/page'
+
+export const metadata = {
+  title: 'Home-1 || FindHouse - Real Estate React Template',
+  description:
+    'FindHouse - Real Estate React Template',
+}
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <HomeMain/>
+    </Wrapper>
+    
+  )
+}
